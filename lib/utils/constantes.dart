feee62912e1743897f8a3a478089cc85208bdc65
RiveraSerg <a321478@alumnos.uaslp.mx>
2024-09-17 22:00:07 -0600

    home, login y constantes, segundo commit

---
+++ lib/utils/constantes.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+const fondo = Color(0xff520101);
+const fondo2 = Color(0xff640101);
+const fondo3 = Color(0xff410101);
+const fondo4 = Color(0xffa90101);
+const titulos = Color(0xff940043);
+const botones = Color(0xffdc0000);
+const efectos = Color(0xff9b0202);
+
+
+const List listaExamen = [
+  ///‘id # edad # dia de cumpleaños # mes de cumpleaños # Nombre # apellidos # descripción # num de estrellas en gris
+  '10#25#15#7#Alice#Smith#Ingeniera en software#3',
+  '11#30#22#3#Bob#Johnson#Médico#4',
+  '12#40#5#10#Carol#Williams#Abogada#2',
+  '13#28#8#11#David#Brown#Diseñador gráfico#5',
+  '14#35#19#1#Emma#Davis#Arquitecta#4',
+  '15#22#12#6#Frank#Miller#Programador#1',
+  '16#33#24#9#Grace#Wilson#Enfermera#3',
+  '17#45#7#2#Henry#Moore#Profesor#4',
+  '18#38#21#4#Isabel#Taylor#Psicóloga#5',
+  '19#27#16#5#Jack#Anderson#Ingeniero civil#2',
+  '20#29#30#12#Kate#Thomas#Administradora#3',
+  '21#31#4#8#Liam#Lee#Chef#1'
+];
+
